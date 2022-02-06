300758a5920ed082dfb2675531edc049d682b7a6
brandonkum <abrandonkum08@gmail.com>
2022-02-06 04:26:03 +0000

    commit

--- src/lib/login.svelte
+++ src/lib/login.svelte
@@ -4,8 +4,8 @@
     let email;
     let password;
 
-    async function register() {
-   	 const { error } = await supabase.auth.signUp({
+    async function loginUser() {
+   	 const { error } = await supabase.auth.signIn({
    		 email,
    		 password
    	 });
@@ -13,11 +13,12 @@
    	 if (error) alert(error.message); // alert if error
     }
 </script>
+
 <div class="card mt-5 m-auto text-center" style="width: 24em;">
     <div class="card-body">
-   	 <h5 class="card-title">Welcome</h5>
-   	 <p class="card-text text-muted">Please sign up to continue...</p>
-   	 <form class="form-floating" on:submit|preventDefault={register}>
+   	 <h5 class="card-title">Welcome Back</h5>
+   	 <p class="card-text text-muted">Please log-in to continue...</p>
+   	 <form class="form-floating" on:submit|preventDefault={loginUser}>
    		 <div class="mb-3">
    			 <label for="emailInput" class="form-label">Email address</label>
    			 <input bind:value={email} type="email" class="form-control" id="emailInput" />
@@ -35,11 +36,12 @@
    		 <div id="passwordHelp" class="form-text">
    			 Your password must be at least 6 characters long.
    		 </div>
-   		 <button type="submit" class="btn btn-dark mt-3">Sign Up</button>
+   		 <button type="submit" class="btn btn-dark mt-3">Login</button>
    	 </form>
     </div>
 </div>
 
 <div class="text-center mt-3">
-    <a href="/login" class="text-muted">Already have an account? Click here to login.</a>
+    <a href="/signup" class="text-muted">First time? Click here to register for an account.</a>
 </div>
+
